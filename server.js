6ae03a04df2eab4afd0526fd6305fd7cd8b5ea59
Dustdeep <130654638+Viste360@users.noreply.github.com>
2025-11-24 18:13:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import cors from "cors";
+app.use(cors());
 const express=require('express');const app=express();
 app.get('/health',(req,res)=>res.send('ok'));
 app.listen(3000,()=>console.log('running'));
